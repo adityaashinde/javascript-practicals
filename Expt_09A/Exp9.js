1c8f9900afd486ec402638359f647ed7b85d1545
Aditya Shinde <adityashinde4580@gmail.com>
2023-11-06 00:05:08 +0530

    change background color of Webpage On mouse over event

---
+++ Expt_09A/Exp9.js
@@ -0,0 +1,7 @@
+//Experiment 9a: JavaScript program to change background color of Webpage On mouse over event
+function changeColor1() {
+  document.body.style.backgroundColor = "red";
+}
+function changeColor2() {
+  document.body.style.backgroundColor = "yellow";
+}
